8967d7da3b858f4372168f7942cdcb77f388b30b
Bart Noppen <bart.noppen@desk02.be>
2014-02-19 15:09:29 +0100

    Update.

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,20 +1,5 @@
-var postsData = [
-  {
-    title: '61 Tonnes of Gold Bullion ...',
-    author: 'Sacha Greif',
-    url: 'http://jessescrossroadscafe.blogspot.com'
-  }, 
-  {
-    title: 'Retail Sales Drop was Enormous-Karl Denninger',
-    author: 'Tom Coleman',
-    url: 'http://test'
-  }, 
-  {
-    title: 'U.S. Currency Weak and About to Crash—Karen Hudes',
-    author: 'Tom Coleman',
-    url: 'http://test'
-  }
-];
 Template.postsList.helpers({
-  posts: postsData
+  posts: function() {
+    return Posts.find();
+  }
 });
